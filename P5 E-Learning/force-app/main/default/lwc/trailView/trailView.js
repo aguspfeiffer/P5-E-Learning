52a3dc7060682ecdf93691e9293a04edb6750aaa
aguspfeiffer <aguspfeiffer@gmail.com>
2022-04-14 14:50:45 -0300

    TrailView sin ProgressBar

--- P5 E-Learning/force-app/main/default/lwc/trailView/trailView.js
+++ P5 E-Learning/force-app/main/default/lwc/trailView/trailView.js
@@ -1,3 +1,32 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import { getSObjectValue } from '@salesforce/apex';
+import NAME_FIELD from '@salesforce/schema/Trail__c.Name';
+import DESCRIPTION_FIELD from '@salesforce/schema/Trail__c.Description__c';
+import TRAILTIME_FIELD from '@salesforce/schema/Trail__c.Trail_Time__c';
+import TRAILSCORE_FIELD from '@salesforce/schema/Trail__c.Trail_Score__c'; 
+import getTrail from '@salesforce/apex/TrailController.getTrail';
 
-export default class TrailView extends LightningElement {}
+
+export default class TrailView extends LightningElement {
+    
+  @api recordId;
+
+  @wire(getTrail, {trailId : '$recordId'})
+  trail;        
+
+    get name() {
+    return this.trail.data? getSObjectValue(this.trail.data, NAME_FIELD) : '';
+    } 
+
+    get description() {
+        return this.trail.data ? getSObjectValue(this.trail.data, DESCRIPTION_FIELD) : '';
+    }
+
+    get trailTime() {
+        return this.trail.data ? getSObjectValue(this.trail.data, TRAILTIME_FIELD) : '';
+    }
+
+    get trailScore() {
+        return this.trail.data ? getSObjectValue(this.trail.data, TRAILSCORE_FIELD) : '';
+    }
+} 

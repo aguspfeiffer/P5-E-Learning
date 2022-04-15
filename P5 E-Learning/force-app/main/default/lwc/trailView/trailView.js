f23b7458b8e4a9ef8067b53193a4d972262e5bab
aguspfeiffer <aguspfeiffer@gmail.com>
2022-04-15 08:22:22 -0300

    Trail View con Card

--- P5 E-Learning/force-app/main/default/lwc/trailView/trailView.js
+++ P5 E-Learning/force-app/main/default/lwc/trailView/trailView.js
@@ -5,6 +5,7 @@ import DESCRIPTION_FIELD from '@salesforce/schema/Trail__c.Description__c';
 import TRAILTIME_FIELD from '@salesforce/schema/Trail__c.Trail_Time__c';
 import TRAILSCORE_FIELD from '@salesforce/schema/Trail__c.Trail_Score__c'; 
 import getTrail from '@salesforce/apex/TrailController.getTrail';
+import getTrailProgress from '@salesforce/apex/TrailController.getTrailProgress';
 
 
 export default class TrailView extends LightningElement {
@@ -29,4 +30,13 @@ export default class TrailView extends LightningElement {
     get trailScore() {
         return this.trail.data ? getSObjectValue(this.trail.data, TRAILSCORE_FIELD) : '';
     }
-} 
+
+    @wire(getTrailProgress, {trailId : '$recordId'})
+    trailProgress;  
+
+   get progress(){
+    if(this.trailProgress.data > 0 && this.trailProgress.data < 100){
+        return this.trailProgress.data;
+    }
+   }
+}   

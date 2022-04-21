6e652907f3754c248112333bfefa437010beecf9
aguspfeiffer <aguspfeiffer@gmail.com>
2022-04-21 15:35:43 -0300

    Module Wrapper Funcionando

--- P5 E-Learning/force-app/main/default/lwc/trailView/trailView.js
+++ P5 E-Learning/force-app/main/default/lwc/trailView/trailView.js
@@ -1,7 +1,6 @@
 import { LightningElement, api, wire } from 'lwc';
 import getTrail from '@salesforce/apex/TrailController.getTrail';
 
-
 export default class TrailView extends LightningElement {
     trailName;
     trailDescription;
@@ -9,55 +8,22 @@ export default class TrailView extends LightningElement {
     trailScore;
     trailCompleted;
     progressTrail;
-    modules;
+    modulesWrapps;
 
   @api recordId;
   @wire(getTrail, {trailId : '$recordId'})
   trail({data, error}){
+   
       if(data){
-          console.log('DATA--> ',data)
-         
         this.trailName = data.trail.Name;
         this.trailDescription = data.trail.Description__c;
         this.trailTime = data.trail.Trail_Time__c;
         this.trailScore = data.trail.Trail_Score__c;
         if(data.progressTrail) {this.progressTrail = data.progressTrail};
-        if(data.progressTrail == 100) {this.trailCompleted = true};
-        console.log('trailompleted--> ',this.trailCompleted)
-        console.log('datamodule--> ',data.modules)
-        this.modules = data.modules;
-        
+        if(data.progressTrail == 100) this.trailCompleted = true;
+        if(data.modulesWrapps) this.modulesWrapps = data.modulesWrapps;
       } else if(error) {
           console.log('entramos en error', error)
       }
   }    
- 
-  //>>>>>>>>>>>>>>>>>METODOS GETTERS UTILIZANDO EL .TRAIL en EL TRAILCONTROLLER<<<<<<<<<<<<<<<<<<<<
-    /* get name() {
-    return this.trail.data? getSObjectValue(this.trail.data, NAME_FIELD) : '';
-    } 
-
-    get description() {
-        return this.trail.data ? getSObjectValue(this.trail.data, DESCRIPTION_FIELD) : '';
-    }
-
-    get trailTime() {
-        return this.trail.data ? getSObjectValue(this.trail.data, TRAILTIME_FIELD) : '';
-    }
-
-    get trailScore() {
-        return this.trail.data ? getSObjectValue(this.trail.data, TRAILSCORE_FIELD) : '';
-    } */
-
-
-    //>>>>>>>>>>>>>>>>>>>>>TRAIL PROGRESS<<<<<<<<<<<<<<<<<<<<<<<
-  /*   @wire(getTrailProgress, {trailId : '$recordId'})
-    trailProgress;  
-
-   get progress(){
-    if(this.trailProgress.data > 0 && this.trailProgress.data < 100)
-        return this.trailProgress.data;   
-   }
-
- */
 }   
